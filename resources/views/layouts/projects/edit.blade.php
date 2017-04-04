07698f056201265b952d2ceca5bed06220e76352
oskarziomek <oskar.ziomek@gmail.com>
2017-04-04 12:41:35 +0200

    Modyfikacja widoku dodawania projektu

--- resources/views/layouts/projects/edit.blade.php
+++ resources/views/layouts/projects/edit.blade.php
@@ -5,30 +5,39 @@
 @stop
 
 @section('content')
+<h2>Dodaj nowy projekt</h2>
+<hr />
      <form method="POST" action="{{ route('project.save') }}">
                         {{ csrf_field() }}
     @if(isset($project)) <input name="id" type="hidden" value="{{$project->id}}"> @endif
+	
+	<label for="nameForm">Nazwa projektu:</label>
     <div class="form-group has-feedback">
-        <input name="name" class="form-control" placeholder="Nazwa">
-        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+        <input name="name" id="nameForm" class="form-control" placeholder="Nazwa">
+        <span class="fa fa-file-text form-control-feedback"></span>
     </div>
       @if ($errors->has('name'))
       <span class="help-block text-red">
          {{ $errors->first('name') }}
       </span>
       @endif
+	  
+	  <label for="descriptionForm">Opis projektu:</label>
       <div class="form-group has-feedback">
-        <input name="description" class="form-control" placeholder="Opis">
-        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+	  <span class="fa fa-pencil form-control-feedback"></span>
+		<textarea name="description" id="descriptionForm" class="form-control" rows="3" 
+			placeholder="Opis..." dir="ltr" style="margin: 0px -7px 0px 0px;  min-width:100%; max-width:100%; min-height: 100px; max-height:200px;"></textarea>        
     </div>
       @if ($errors->has('description'))
       <span class="help-block text-red">
          {{ $errors->first('description') }}
       </span>
       @endif
-      <div class="form-group has-feedback">
+	  
+		<label for="managerForm">Wybierz menedżera:</label>
+		<div class="form-group has-feedback">
 
-        <select name="manager" class="form-control">
+        <select name="manager" id="managerForm" class="form-control">
             @foreach(\App\User::whereRole('manager')->get() as $m)
                 <option value="{{$m->id}}">{{$m->name}} {{$m->surname}}</option>
             @endforeach
